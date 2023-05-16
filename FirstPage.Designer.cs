5101bbcd82e8a52e9be240e8e9b138cc8db23064
Ferrariedhgs <Ferrariedhgs@gmail.com>
2023-05-16 16:41:13 +0300

    merge acum?

--- FirstPage.Designer.cs
+++ FirstPage.Designer.cs
@@ -31,8 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FirstPage));
             pictureBox2 = new PictureBox();
             panel1 = new Panel();
-            button1 = new Button();
+            button3 = new Button();
             button2 = new Button();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             panel1.SuspendLayout();
             SuspendLayout();
@@ -51,6 +52,7 @@
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(192, 192, 255);
+            panel1.Controls.Add(button3);
             panel1.Controls.Add(button2);
             panel1.Controls.Add(button1);
             panel1.Location = new Point(14, 16);
@@ -59,6 +61,35 @@
             panel1.Size = new Size(1669, 133);
             panel1.TabIndex = 2;
             panel1.Paint += panel1_Paint;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 128, 128);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(231, 20);
+            button3.Name = "button3";
+            button3.Size = new Size(223, 91);
+            button3.TabIndex = 1;
+            button3.Text = "Programe de studii";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 128);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(460, 20);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(208, 91);
+            button2.TabIndex = 3;
+            button2.Text = "Note";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // button1
             // 
@@ -74,21 +105,6 @@
             button1.Text = "Cicluri de învățământ";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.FromArgb(255, 128, 128);
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.Location = new Point(527, 20);
-            button2.Margin = new Padding(3, 4, 3, 4);
-            button2.Name = "button2";
-            button2.Size = new Size(208, 91);
-            button2.TabIndex = 3;
-            button2.Text = "Note";
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
             // 
             // FirstPage
             // 
@@ -114,5 +130,6 @@
         private Panel panel1;
         private Button button1;
         private Button button2;
+        private Button button3;
     }
 }
